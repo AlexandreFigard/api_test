0d432dfe4a6de89fa3c5a9a6acb608ac3ceeac2d
AlexF <84327508+AlexandreFigard@users.noreply.github.com>
2021-06-18 12:09:27 +0200

    Update app_test.js

--- app_test.js
+++ app_test.js
@@ -25,7 +25,6 @@ app.post('/parkings', (req,res) => {
   pes.status(200).json(parkings);
 })
 
- 
 // route put parkings
 app.put('/parkings/:id', (req,res) => {
   const id = parseInt(req.params.id);
